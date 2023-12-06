5988b6eb79da712984fb9bb1d058d5a404e056e5
Rehmat Ali <89012464+Rahi776@users.noreply.github.com>
2023-12-07 02:53:17 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -73,12 +73,3 @@ resource "aws_api_gateway_deployment" "api" {
   stage_name  = "prod"
 }
 
-output "lambda_function_arn" {
-  description = "The ARN (Amazon Resource Name) of the AWS Lambda function."
-  value       = aws_lambda_function.hello_world_lambda.arn
-}
-
-output "lambda_function_invoke_url" {
-  description = "The URL to invoke the AWS Lambda function."
-  value       = aws_api_gateway_deployment.api.invoke_url
-}
