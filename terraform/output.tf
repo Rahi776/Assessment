3c549e9c40bf5cffae2923aced68cbf492affe23
Rehmat Ali <89012464+Rahi776@users.noreply.github.com>
2023-12-07 05:01:39 +0500

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,10 +1,8 @@
 output "lambda_function_arn" {
-  description = "The ARN (Amazon Resource Name) of the AWS Lambda function."
-  value       = aws_lambda_function.hello_world_lambda.arn
+  value = aws_lambda_function.hello_world_lambda.arn
 }
 
 output "lambda_function_invoke_url" {
-  description = "The URL to invoke the AWS Lambda function."
-  value       = aws_api_gateway_deployment.deployment.invoke_url
+  value = aws_api_gateway_deployment.deployment.invoke_url
 }
 
