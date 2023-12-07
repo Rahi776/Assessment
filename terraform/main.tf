473053cf3c014648f9a2787d402e32ea11a01b9e
Rehmat Ali <89012464+Rahi776@users.noreply.github.com>
2023-12-07 05:09:16 +0500

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,16 @@ resource "aws_api_gateway_method_response" "response" {
   }
 }
 
+resource "aws_api_gateway_integration_response" "integration_response" {
+  rest_api_id           = aws_api_gateway_rest_api.api.id
+  resource_id           = aws_api_gateway_resource.resource.id
+  http_method           = aws_api_gateway_method.method.http_method
+  status_code           = aws_api_gateway_method_response.response.status_code
+  response_templates    = {
+    "application/json" = ""
+  }
+}
+
 resource "aws_api_gateway_deployment" "deployment" {
   depends_on = [aws_api_gateway_integration.integration]
 
